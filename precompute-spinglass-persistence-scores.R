506011929c85489a02978b7e7e55319fc7dea32a
Carl Pearson <cap10@ufl.edu>
2016-03-06 13:21:47 -0500

    build score files by interval

--- precompute-spinglass-persistence-scores.R
+++ precompute-spinglass-persistence-scores.R
@@ -5,21 +5,37 @@
 require(data.table)
 require(igraph)
 require(parallel)
+require(optparse)
 
 rm(list=ls())
 
-args <- commandArgs(trailingOnly = T)
-# args <- c("input/background-clusters/spin-glass/30-30","input/background-clusters/spin-glass/30-30-acc/01.rds")
+parse_args <- function(argv = commandArgs(trailingOnly = T)) {
+  parser <- optparse::OptionParser(
+    usage = "usage: %prog path/to/community-interval.rds path/to/output-useratob-scores.rds",
+    description = "convert (user, community) into (user.a in community X, user.b in community X, 1).",
+    option_list = list(
+      optparse::make_option(
+        c("--verbose","-v"),  action="store_true", default = FALSE,
+        help="verbose?"
+      )
+    )
+  )
+  req_pos <- list(input.dt=readRDS, outputfn=identity)
+  parsed <- optparse::parse_args(parser, argv, positional_arguments = length(req_pos))
+  result <- c(mapply(function(f,c) f(c), req_pos, parsed$args, SIMPLIFY = F), parsed$options)
+  if(result$verbose) print(result)
+  result
+}
 
-precomms <- readRDS(args[1])
-n <- precomms[,max(interval)]
+with(parse_args(
+#  c("input/background-clusters/spin-glass/base-30-30/001.rds","input/background-clusters/spin-glass/acc-30-30/001.rds") # uncomment for debugging
+),{
+  res <- if (dim(input.dt)[1]) input.dt[, {
+      tmp <- combn(user_id, 2)
+      list(user.a = tmp[1,], user.b = tmp[2,], score = 1)
+    },
+    by=list(community)
+  ] else data.table(community=integer(0), user.a = integer(0), user.b=integer(0), score=integer(0))
 
-crs <- min(as.integer(Sys.getenv("PBS_NUM_PPN")), detectCores(), na.rm = T)
-
-mclapply(1:n, function(ntrvl){
-  saveRDS(precomms[interval == ntrvl, {
-    tmp <- combn(user_id, 2)
-    list(user.a = tmp[1,], user.b = tmp[2,], interval=ntrvl, score = 1)
-  },
-  by=list(community)], sprintf("%s/%02d.rds", args[2], ntrvl))
-}, mc.cores = crs)
+  saveRDS(res, outputfn)
+})
