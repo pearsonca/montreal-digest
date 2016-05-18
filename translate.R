48940c8fdedec96c203b73a40d5e7be4f3740aa8
Carl Pearson <cap10@ufl.edu>
2016-05-18 12:48:13 -0400

    update translation script to cat

--- translate.R
+++ translate.R
@@ -1,7 +1,7 @@
 # translate.R raw sim output => new user_ids (negatives), original location ids, correct times
 
 args <- commandArgs(trailingOnly = T)
-# args <- c("input/remap-location-ids.rds", "output/matched/mid/lo/late/10/001-covert-out.csv", "output/matched/mid/lo/late/10/001-covert-trans.csv", "20649600")
+# args <- c("input/remap-location-ids.rds", "output/matched/mid/lo/late/10/001-covert-out.csv", "20649600")
 
 
 require(data.table)
@@ -11,8 +11,8 @@ orig <- fread(args[2])
 
 orig$V2 <- location_remap[orig$V2, location_id]
 orig$V1 <- -orig$V1
-offset <- as.integer(args[4])
+offset <- as.integer(args[3])
 orig$V3 <- orig$V3 + offset
 orig$V4 <- orig$V4 + offset
 
-write.table(orig, args[3], row.names = F, col.names = F, sep = ",", quote = F)
+write.table(orig, file = stdout(), row.names = F, col.names = F, sep = ",", quote = F)
