0bad7eee9c46910ecb75de01aa6d915e17a80ceb
Carl Pearson <cap10@ufl.edu>
2016-07-08 14:11:21 -0400

    slurm tweak

--- background/pc_pbs.sh
+++ background/pc_pbs.sh
@@ -1,15 +1,14 @@
 cat <<EOF
 #!/bin/bash
-#SBATCH -r n
 #SBATCH --job-name=$1
-#SBATCH -o $1.o
-#SBATCH -e $1.err
+#SBATCH -o $1.o-%
+#SBATCH -e $1.err-%a
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=cap10@ufl.edu
 #SBATCH -t 36:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH -N8
-#SBATCH --mem-per-cpu=4gb
+#SBATCH --cpus-per-task=8
+#SBATCH -N1
+#SBATCH --mem-per-cpu=12gb
 #SBATCH --array=1-$3
 
 module load gcc/5.2.0 R/3.2.2
